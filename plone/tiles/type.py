e57c908da7bf2af16ebda49f20795065adc3642c
cewing <cris@crisewing.com>
2012-01-03 12:16:18 -0800

    * adding support for separate edit and delete permissions to the tile directive.  edit defaults to cmf.ModifyPortalContent, delete defaults to same as add
    refs #173

--- plone/tiles/type.py
+++ plone/tiles/type.py
@@ -1,18 +1,22 @@
 from zope.interface import implements
 from plone.tiles.interfaces import ITileType
 
+
 class TileType(object):
-    """A utility that describes a type of tile 
+    """A utility that describes a type of tile
     """
-    
+
     implements(ITileType)
-    
-    def __init__(self, name, title, add_permission, description=None, schema=None):
+
+    def __init__(self, name, title, add_permission, edit_permission,
+                 delete_permission, description=None, schema=None):
         self.__name__ = name
         self.title = title
         self.add_permission = add_permission
+        self.edit_permission = edit_permission
+        self.delete_permission = delete_permission
         self.description = description
         self.schema = schema
-    
+
     def __repr__(self):
-        return u"<TileType %s (%s)>" % (self.__name__, self.title,)
+        return u"<TileType %s (%s)>" % (self.__name__, self.title, )
